bc99b497f8988a9689f35b4e56f683e707ad345a
Jérémie <40383724+Jeremiejade@users.noreply.github.com>
2023-08-01 16:45:58 +0200

    [TECH] et tique. (#19)

--- e.test.js
+++ e.test.js
@@ -1,6 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import { e } from './e';
 
+// mon commentaire est plus long que ça
+
 describe('#e', () => {
   it('should return e', async () => {
     return new Promise((resolve) => {
