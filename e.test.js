cd435ae6347d8db94b4bd671a757ef6d83ad6f92
jeremie <jeremie.jade@pix.fr>
2023-08-01 16:42:19 +0200

    un commit

--- e.test.js
+++ e.test.js
@@ -1,6 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import { e } from './e';
 
+// mon commentaire
+
 describe('#e', () => {
   it('should return e', async () => {
     return new Promise((resolve) => {
