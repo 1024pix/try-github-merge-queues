c53dbdd71508f0230e8d5cac68e53494777ebe02
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2023-08-01 16:42:38 +0200

    remove a comments

--- a.test.js
+++ a.test.js
@@ -1,9 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { a } from './a';
 
-// first commit
-// second commit
-
 describe('#a', () => {
   it('should return a', () => {
     expect(a()).toEqual('a');
