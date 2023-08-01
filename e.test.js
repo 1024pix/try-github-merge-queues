dd96a94f2214d02bf0f79bd9f797709c46776c3f
jeremie <jeremie.jade@pix.fr>
2023-08-01 16:48:47 +0200

    remove com

--- e.test.js
+++ e.test.js
@@ -1,8 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { e } from './e';
 
-// mon commentaire est plus long que ça
-
 describe('#e', () => {
   it('should return e', async () => {
     return new Promise((resolve) => {
